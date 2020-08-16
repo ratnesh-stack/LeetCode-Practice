cbc870563cb562acfa74deb8fa1b5720228bca80
ratneshShukla <ratneshkumarshukla07@gmail.com>
2020-08-16 20:40:23 +0700

    1544	 Make The String Great
    Code submission accepted.

--- 1544MakeTheStringGreat.playground/Contents.swift
+++ 1544MakeTheStringGreat.playground/Contents.swift
@@ -3,21 +3,23 @@ import UIKit
 func makeGood(_ s: String) -> String {
     
     var goodString = s
-    
-    for index in goodString.indices {
-        let current = goodString[index]
-        
-        let nextIndex = goodString.index(goodString.startIndex, offsetBy: 1)
-        
-        let next = goodString[nextIndex]
-        
-        if (current == next) {
-            let range = goodString.index(goodString.startIndex, offsetBy: 2)..<goodString.endIndex
-            print(range)
-            goodString.removeSubrange(range)
+    var repeatAgain = false
+
+    repeat {
+        repeatAgain = false
+        for index in goodString.indices where goodString.indices.count > 0 && (index < goodString.indices.last!) {
+            let current = goodString[index]
+            let nextIndex = goodString.index(index, offsetBy: 1)
+            let next = goodString[nextIndex]
+            
+            if current != next && ((current.lowercased() == String(next)) || (String(current) == next.lowercased())) {
+                goodString.remove(at: index)
+                goodString.remove(at: index)
+                repeatAgain = true
+            }
         }
-    }
+    } while repeatAgain
     return goodString
 }
 
-makeGood("leEeetcode")
+print(makeGood("abBAcC"))
